1b389382ae4450960dd0b9a29b479d5b78f5dd25
ledinhdat710 <80235959+ledinhdat710@users.noreply.github.com>
2024-02-17 16:00:13 +0700

    responsive dialog info job

--- src/app/components/common/project-item/project-detail/project-detail.component.ts
+++ src/app/components/common/project-item/project-detail/project-detail.component.ts
@@ -12,6 +12,7 @@ export class ProjectDetailComponent implements OnInit {
   @Input() display: boolean = false;
   styleDialog: any = { width: '50vw' };
   widthDialog: string = '800px';
+  widthDialogJob: string = '35vw';
   widthBox1: string = '590px';
   widthBox2: string = '642px';
   innerWidth: any;
@@ -60,6 +61,7 @@ export class ProjectDetailComponent implements OnInit {
       this.widthDialog = '400px';
       this.widthBox1 = '250px';
       this.widthBox2 = '300px';
+      this.widthDialogJob = '70vw'
     }
   }
 
